e843fe8f5d35693b32dcddf77af017b948536bd3
Matthew Stiles <git@stiles.dev>
2023-01-15 01:24:19 -0500

    fix: set vue script baseIndent to 0

--- packages/eslint-config-nuxt/rules.js
+++ packages/eslint-config-nuxt/rules.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     'vue/component-name-in-template-casing': ['error', 'PascalCase', { registeredComponentsOnly: false }],
     'vue/html-indent': ['error', 4],
-    'vue/script-indent': ['error', 4, { baseIndent: 1, switchCase: 1 }],
+    'vue/script-indent': ['error', 4, { baseIndent: 0, switchCase: 1 }],
     'vue/max-attributes-per-line': ['error', { singleline: 5, multiline: 1 }],
     'vue/no-v-for-template-key': 'off',
     'vue/no-v-model-argument': 'off',
